4e88dbf595745ea6302bebd9975be43fcc163e4c
Prasannacharya-3151 <prasannacharya428@gmail.com>
2025-09-11 22:32:48 +0530

    authcontroller

---
+++ src/controllers/authController.ts
@@ -0,0 +1,74 @@
+import { Request, Response } from "express";
+import {UserModel} from "../models/User.js"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+
+export const login = async (req: Request, res:Response ) => {
+    try {
+        const { username, userId, password, role } = req.body;
+        
+        //timmed values
+        const trimmedUsername = username?.trim();
+        const trimmedUserId = userId?.trim();
+        const trimmedPassword = password?.trim();
+        const trimmedRole = role?.trim(); 
+
+
+        //checking requird fields
+        if (!trimmedUsername || !trimmedUserId || !trimmedPassword || !trimmedRole){
+            return res.status(400).json({
+                msg:"All the fields are required"
+            })
+        }
+        //checks the role matching with the userId
+        if(
+            (trimmedRole === "student" && !/^4AL\d{2}[A-Z]{2}\d{3}$/.test(trimmedUserId)) ||
+            (trimmedRole === "faculty" && !/^FG\d{3}$/.test(trimmedUserId)) ||
+            (trimmedRole === "hod" && !/^HOD\d{2,}$/.test(trimmedUserId))
+        ) {
+            return res.status(400).json({
+                msg:"Invalid ID formate for selected role"
+            })
+        }
+        //finding the user
+        const user = await UserModel.findOne({
+            userId: trimmedUserId, 
+            role: trimmedRole
+        })
+        if(!user){
+            return res.status(400).json({
+                msg:"user not found or a role mismatch"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        //comparing the password
+        const isMatch = await bcrypt.compare(hashedPassword, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({
+                msg:"Invalid password"
+            })
+        }
+
+        //generating the token
+
+        const token = jwt.sign(
+            {id : user._id, role:user.role},
+            process.env.JWT_SECRET || "secret",
+            { expiresIn : "1d" }
+        )
+
+        return res.json({
+            msg:"Login successful",
+            token,
+            user: {userId :user.userId, name: user.username, role:user.role },
+        })
+    } catch(error){
+        console.error("Login error ", error)
+        return res.status(500).json({
+            msg:"server error", error
+        })
+    }
+}
--- src/models/User.ts
+++ src/models/User.ts
@@ -8,10 +8,12 @@ interface User {
 }
 
 const userSchema = new Schema({
-    username:{type: String, required: true},
-    userId:{type:String, require:true},
-    password:{type:String, required:true },
-    role:{type: String, enum:["student", "faculty", "hod"], required:true}
-})
+    username:{type: String, required: true, trim: true},
+    userId:{type:String, require:true, unique: true, trim :true},
+    password:{type:String, required:true, trim :true},
+    role:{type: String, enum:["student", "faculty", "hod"], required:true, trim:true}
+},
+ { timestamps: true }
+)
 
 export const UserModel = model("userSchema", userSchema)
