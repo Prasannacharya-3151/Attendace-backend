d493ccddf02abe965e555b3ba97b8d0c4d738ddf
Prasannacharya-3151 <prasannacharya428@gmail.com>
2025-09-11 16:47:11 +0530

    env

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async() =>{
     try{
-        await mongoose.connect(process.env.MONGO_URL || "");
-        console.log("mongoDB connected");
+        await mongoose.connect(process.env.MONGO_URL || "");  //|| "" fallback(empty string) if MONGO_URL is missing, so code wont crash at runtime but it will throw an error 
+        console.log("mongoDB connected");//redas the MongoDB connection string (URL) from our .env file 
     } catch (error){
         console.error("mongoDB connection failed:",error)
-        process.exit(1);
+        process.exit(1);//doesnt keep running without a db 
     }
 }
